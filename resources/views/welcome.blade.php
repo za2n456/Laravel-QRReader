1c812a98688271a8980971a6bfccd509dc072182
Zazan Pratama <mail@zazan.me>
2020-06-19 13:30:13 +0700

    card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,7 @@
 <div class="container">
   <div class="row">
   <div class="col-lg-12">
-    <div class="card">
-      <div class="card-header d-flex">
-          <div class="col-lg-9">
-              Input Pasien
-          </div>
-          <div class="col-lg-3 text-right">
-              <a class="btn btn-light btn-sm" href="{{ route('pasien') }}"><i class="fa fa-fw fa-reply-all" aria-hidden="true"></i><span class="hidden-xs"> Back</span></a>
-          </div>
-      </div>
+    <div class="cards">
       <div class="card-body">
         <form action="{{ route('pasien.store') }}" method="POST">
       		@csrf
